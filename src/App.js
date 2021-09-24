d84348dbabbc01b971f3ff0cd4b54cc1705c8711
Ishant Sahu <ishant.27june@gmail.com>
2021-09-24 13:27:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <main>
+        <header>
+          <h1>My website</h1>
+        </header>
+        <section>
+          <article>
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+              molestias ex officia eligendi reiciendis fuga magnam? Ullam ut ab
+              doloremque accusamus iusto aliquam facilis sapiente!
+            </p>
+          </article>
+          <article>
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+              molestias ex officia eligendi reiciendis fuga magnam? Ullam ut ab
+              doloremque accusamus iusto aliquam facilis sapiente!
+            </p>
+          </article>
+        </section>
+        <aside>
+          <div class="module">
+            <h3>Module</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
+              rem ab delectus fugit repellendus perspiciatis dolor consequuntur
+              tenetur voluptatem a vitae odit aspernatur? Voluptas quisquam
+              corporis nostrum aspernatur aliquid harum saepe ab pariatur veniam
+              iste ipsam alias nemo voluptatibus doloribus.
+            </p>
+          </div>
+        </aside>
+      </main>
+    </div>
+  );
+}
